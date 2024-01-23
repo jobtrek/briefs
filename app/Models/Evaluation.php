5524cd5f0658ddc606d07c304728c68759a84294
Bilaljanir <bilaljanir@gmail.com>
2024-01-23 10:21:57 +0100

    add Evaluation model and migration

---
+++ app/Models/Evaluation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Evaluation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'date_evaluation',
+        'commentaire_general_mandat',
+        'brief_id',
+        'criteria_id',
+        'evaluation_criteria_id',
+    ];
+
+    public function brief(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(brief::class);
+    }
+
+    public function criteria(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Criteria::class);
+    }
+
+    public function EvaluationCriteria(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(EvaluationCriteria::class);
+    }
+}
