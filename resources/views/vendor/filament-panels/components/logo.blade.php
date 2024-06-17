efd471504bc836007014a299de9992def1fd47a9
Bilaljanir <bilaljanir@gmail.com>
2024-06-17 10:57:19 +0200

    Adapt logo for light and dark mode

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,9 +1,37 @@
-<img
+<div id="logo-container">
+    <img
+        id="logo-dark"
+        src="https://jobtrek.ch/wp-content/themes/jobtrek-theme/static/img/logo-white.svg"
+        class="h-11 hidden dark:block"
+        alt="logo jobtrek"
+    >
+    <img
+        id="logo-light"
+        src="https://jobtrek.ch/wp-content/themes/jobtrek-theme/static/img/logo-black.svg"
+        class="h-11 block dark:hidden"
+        alt="logo jobtrek"
+    >
+</div>
 
-    src="https://jobtrek.ch/wp-content/themes/jobtrek-theme/static/img/logo-white.svg"
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const logoDark = document.getElementById('logo-dark');
+        const logoLight = document.getElementById('logo-light');
 
-    class="h-11"
+        const updateLogo = () => {
+            if (document.documentElement.classList.contains('dark')) {
+                logoDark.classList.remove('hidden');
+                logoLight.classList.add('hidden');
+            } else {
+                logoDark.classList.add('hidden');
+                logoLight.classList.remove('hidden');
+            }
+        };
 
- alt="logo jobtrek"
+        updateLogo();
 
->
+        const observer = new MutationObserver(updateLogo);
+        observer.observe(document.documentElement, { attributes: true, attributeFilter: ['class'] });
+    });
+</script>
+
