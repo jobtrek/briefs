14be80f57c895a665d3a84008f0af922fc3c3a39
Bilaljanir <bilaljanir@gmail.com>
2024-06-28 14:02:01 +0200

    feat: Add settings page part1

---
+++ lang/vendor/filament-settings-hub/en/messages.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'title' => 'Paramètre',
+    'group' => 'la page en développement',
+    'back' => 'Back',
+    'Paramètre' => [
+        'site' => [
+            'title' => 'Site Settings',
+            'description' => 'Manage your site settings',
+            'form' => [
+                'site_name' => 'Site Name',
+                'site_description' => 'Site Description',
+                'site_logo' => 'Site Logo',
+                'site_profile' => 'Site Profile Image',
+                'site_keywords' => 'Site Keywords',
+                'site_email' => 'Site Email',
+                'site_phone' => 'Site Phone',
+                'site_author' => 'Site Author',
+            ],
+            'site-map' => 'Generate Site Map',
+            'site-map-notification' => 'Site Map Generated Successfully',
+        ],
+        'location' => [
+            'title' => 'Location Settings',
+            'description' => 'Manage your location settings',
+            'form' => [
+                'site_address' => 'Site Address',
+                'site_phone_code' => 'Site Phone Code',
+                'site_location' => 'Site Location',
+                'site_currency' => 'Site Currency',
+                'site_language' => 'Site Language',
+            ],
+        ],
+    ],
+];
